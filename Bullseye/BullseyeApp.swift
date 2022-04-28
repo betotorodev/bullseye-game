46e3d417739d7a4ca23454ea2fcbcba668e5ff64
Beto Toro <beto.toro@zemoga.com>
2022-04-28 09:35:20 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Beto Toro on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
